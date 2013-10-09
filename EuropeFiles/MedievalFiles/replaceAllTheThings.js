083e94a339228199620dc282e8b3540efcc37fcb
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-10-09 12:02:19 -0400

    first push

---
+++ EuropeFiles/MedievalFiles/replaceAllTheThings.js
@@ -0,0 +1,30 @@
+(function()
+{
+  var forEach = Array.prototype.forEach;  
+
+  var replaceLinks = function(elem, ind, arr)
+  {
+    if (elem.hasAttribute("src")) 
+    { 
+        if (elem.getAttribute("src").substring(0,1) == "/") 
+        {
+            elem.setAttribute("src", document.ROOT_URL+elem.getAttribute("src")); 
+        }
+    };
+
+    if (elem.hasAttribute("href")) 
+    { 
+        if (elem.getAttribute("href").substring(0,1) == "/") 
+        {
+            elem.setAttribute("href", document.ROOT_URL+elem.getAttribute("href"));
+        }
+    };
+  };
+
+  document.observe("dom:loaded", function()
+  {
+    forEach.call(document.getElementsByTagName("a"), replaceLinks);
+    forEach.call(document.getElementsByTagName("img"), replaceLinks);
+  });
+}
+)();
